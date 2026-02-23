23c426cc708ad7ea896e0b4d5973898eea03e731
Mariia Koliako <mariia.koliiako0131119@gmail.com>
2026-02-23 13:36:38 +0300

    commit new

--- lesson_1/lesson_1_1.py
+++ lesson_1/lesson_1_1.py
@@ -1,3 +1,21 @@
-print()
-print()
-print(a)
+# a = 1
+# b = a
+# # b = 8.2
+# # print(a)
+# str_1 = "Легко в IT"
+# str_1 = 8
+# print(b)
+# # print(type(str_1))
+
+# a, b = 8, 10
+# a, b = b, a
+# print(a)
+# print(b)
+
+# a = 5
+# a = c = 4
+# b = d = "что нибудь", "вторая строка"
+# print(id(a))
+# print(id(b))
+# print(id(c))
+# print(id(d))

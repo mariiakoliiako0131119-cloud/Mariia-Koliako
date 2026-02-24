6901556e691ab0242a006ba1c1b36cdce3d2a0c9
Mariia Koliako <mariia.koliiako0131119@gmail.com>
2026-02-24 16:21:06 +0300

    commit

---
+++ lesson_2/homework lesson 2_2_2.py
@@ -0,0 +1,115 @@
+""" Задание 3 """
+from itertools import count
+from queue import PriorityQueue
+from xml.etree.ElementPath import prepare_parent
+
+# a = 8
+# b = 3
+# x = 2.5
+# y = - 1.7
+# print(type(a))
+# print(type(b))
+# print(type(x))
+# print(type(y))
+
+""" Задание 2 """
+# num1 = 10
+# num2 = 5
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 // num2)
+# print(num1 % num2)
+# print(num1 ** num2)
+
+""" Задание 3 """
+# a = 10
+# b = 3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# a = - 10
+# b = 3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# a = 10
+# b = -3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+""" Задание 4 """
+# a = 5 + 3 * 2
+# b = (5 + 3) * 2
+# c = 10 / 2 ** 2
+# print(a)
+# print(b)
+# print(c)
+
+""" Задание 5 """
+# count = 10
+# count += 5
+# count -= 3
+# count *= 2
+# count /= 4
+# print(count)
+
+""" СТРОКИ - Задание 1 """
+# s1 = "Python"
+# s2 = 'Программирование'
+# print(s1)
+# print(s2)
+# s3 = """
+# Мария
+# Коляко
+# """
+# print(s3)
+# s3 = ""
+# print(len(s3))
+
+""" Задание 2 """
+# first_name = "Иван"
+# last_name = "Петров"
+# full_name = first_name + " " + last_name
+# print(full_name)
+
+""" Задание 3 """
+# s = "Возраст"
+# age = 25
+# a = s + " " + str(age)
+# print(type(a))
+
+""" Задание 4 """
+# a = "Мария"
+# # b = (a + " ") * 5
+# c = (a + " ") * 2.5
+# print(b)
+
+""" Задание 5 """
+# text = "Привет, мир!"
+# print(len(text))
+# a = " "
+# print(len(a))
+
+""" Задание 6 """
+# sentence = "Я изучаю Python"
+# print("Python" in sentence)
+# print("Java" in sentence)
+
+""" Задание 7 """
+# a = "apple"
+# b = "banana"
+# print(a > b)
+# print(a < b)
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+# print(a <= b)
+
+# print(ord('A'))
+# print(ord('a'))
+# print(ord('Я'))

b859f31174ffb05999097fba93028ef2f65b539e
Mariia Koliako <mariia.koliiako0131119@gmail.com>
2026-02-25 19:33:33 +0300

    commit

---
+++ lesson_3/Homework_lesson_3.py
@@ -0,0 +1,118 @@
+""" Задание 1 """
+from queue import PriorityQueue
+
+# print("Привет, мир!")
+# print(5, 10, 15)
+# print(10 + 25)
+
+""" Задание 2 """
+# a = 1
+# b = 2
+# c = 3
+# print(a, b, c, sep=" & ")
+# s1 = "Python"
+# s2 = "лучший язык"
+# print(s1, s2, sep="  ")
+# print(s1, s2, sep=" ", end="")
+
+""" Задание 3 """
+# x = 3.14
+# y = -8
+# print(f"Координаты точки: x = {x} и y = {y}")
+# name = input()
+# age = input()
+# print(f"Имя: {name}, Возраст {age}")
+
+""" Задание 4 """
+
+# name = input()
+# print("Привет", name)
+
+""" Задание 5 """
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# print(f"Итого: {a + b}")
+# number = int(input("Введите целое число: "))
+# print(number ** 2)
+
+""" Булевые значения - Задание 1 """
+# print(5 > 3)
+# print(10 < 2)
+# print(7 == 7)
+# print(6 != 8)
+# print(4 >= 4)
+# print(9 <= 3)
+# a = 8
+# b = 12
+# res = a > b
+# print(type(res))
+
+""" Задание 2 """
+# x = 15
+# print(x % 2 == 0)
+# print(x == x // 5)
+# print(x == x % 3, x % 5)
+
+""" Задание 3 """
+# y = 4.5
+# print(y <= 1 and y >= 10)
+# print(1 <= y >= 10)
+# print(0 >= y >= 5) or (10 >= y >= 15)
+# print(not(0 >= y >= 5) or (10 >= y >= 15))
+
+""" Задание 4 """
+
+# НЕ ПОНИМАЮ ЗАДАНИЕ
+
+""" Задание 5 """
+# print(bool(0))
+# print(bool(-5))
+# print(bool(3.14))
+# print(bool(""))
+# print(bool("Python"))
+# print(bool(" "))
+
+# n = 10
+# print(bool(n))
+# print(n % 5 == 0)
+# print(n == n / 3)
+
+""" Срезы - Задание 1 """
+s = "Программирование"
+# print(s[0])
+# print(s[len(s) -1])
+# print(s[2])
+# print(s[-2])
+
+""" Задание 2 """
+# print(s[100])
+# print(s[len(s) -1])
+
+""" Задание 3 """
+# part = (s[:6])
+# print(part)
+# part1 = (s[-5:])
+# print(part1)
+# part2 = (s[3:7])
+# print(part2)
+# part3 = (s[::2])
+# print(part3)
+# part4 = (s[::-1])
+# print(part4)
+
+""" Задание 4 """
+# print((s[0::3]))
+# print(s[::-2])
+
+""" Задание 5 """
+# print(s[0]) у меня нет ошибки
+# s2 = "П"
+# print(s2 + s[1:])
+
+""" Задание 6 """
+word = "abcdefgh"
+print(word[2:5])
+print(word[::-1])
+print(word[1:-1])
+
+
